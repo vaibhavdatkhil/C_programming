8257e348f0c37f69dd3f51703ccc343e5b60f1be
vaibhavdatkhil <vaibhavdatkhil@gmail.com>
2025-10-03 23:26:12 +0530

    Add files via upload

---
+++ FunctionDemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int Value1 = 0, Value2 = 0, Ans = 0;
+
+    Value1 = 10;
+    Value2 = 11;
+
+    Ans = Value1 + Value2;
+
+    printf("Addition is : %d\n",Ans);
+
+    return 0;
+}
