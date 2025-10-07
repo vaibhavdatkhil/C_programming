e624e1fa7694693da38d508fd0f97d3f8c73fcab
Vaibhav Datkhil <vaibhavdatkhil@gmail.com>
2025-10-07 23:15:16 +0530

    initial commit

---
+++ ArrayDemo5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    double Arr[] = {10.0,20.0,30.0,40.0};
+
+    printf("Size of Arr : %d \n",sizeof(Arr));   // 32
+
+    //Arr++;      // Error
+
+    return 0;
+}
